6983d00b94049749c403c7ed7b61ac859d95483e
rafaatsouza <souza.t.rafael@gmail.com>
2020-09-18 15:37:00 -0300

    Adds summaries

--- Source/ErosionFinder/Dtos/DocumentWithExcludedListCheck.cs
+++ Source/ErosionFinder/Dtos/DocumentWithExcludedListCheck.cs
@@ -4,8 +4,14 @@ namespace ErosionFinder.Dtos
 {
     internal class DocumentWithExcludedListCheck
     {
+        /// <summary>
+        /// Document retrieved from the solution
+        /// </summary>
         public Document Document { get; set; }
 
+        /// <summary>
+        /// Flag that indicates if the document is in excluded list
+        /// </summary>
         public bool IsInExcludedList { get; set; }
     }
 }
