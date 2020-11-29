fdd6d4b3286819b9e95928d725c20c9e2e9687ca
Rafael Torres <souza.t.rafael@gmail.com>
2020-11-29 16:58:51 -0300

    Adds new tests

--- Source/ErosionFinder.Tests/ConstraintsAndViolationsMethodsTest.cs
+++ Source/ErosionFinder.Tests/ConstraintsAndViolationsMethodsTest.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -97,10 +98,50 @@ namespace ErosionFinder.Tests
 
             Assert.Equal(nameof(ConstraintsError.NamespaceNotFoundForLayer), result.Key);
         }
-    
+
         [Fact]
-        [Trait(nameof(ConstraintsAndViolationsMethods.GetViolations), "Success")]
-        public void GetViolations_Success()
+        [Trait(nameof(ConstraintsAndViolationsMethods.GetViolations), "Error_NamespaceNotFoundForRegexDefinedLayer")]
+        public void GetViolations_Error_NamespaceNotFoundForRegexDefinedLayer()
+        {
+            var originLayerRegex = new Regex(@"(Test)(.+)(\w*(Origin([s]{1})?)\b)");
+            var targetLayerRegex = new Regex(@"(Test)(.+)(\w*(Target([s]{1})?)\b)");
+
+            var constraints = new ArchitecturalConstraints()
+            {
+                Rules = new List<ArchitecturalRule>()
+                {
+                    new ArchitecturalRule()
+                    {
+                        OriginLayer = "Origin",
+                        TargetLayer = "Target",
+                        RuleOperator = RuleOperator.NeedToRelate
+                    }
+                },
+                Layers = new Dictionary<string, NamespacesGroupingMethod>()
+                {
+                    { "Origin", new NamespacesRegularExpressionGrouped(originLayerRegex) },
+                    { "Target", new NamespacesRegularExpressionGrouped(targetLayerRegex) }
+                }
+            };
+
+            var codeFiles = new List<CodeFile>()
+            {
+                GetCodeFileWithSingleClassStructure("Test", "TestNamespace", null, null)
+            };
+
+            var result = Assert.Throws<ConstraintsException>(() =>
+            {
+                ConstraintsAndViolationsMethods.GetViolations(constraints, codeFiles, default);
+            });
+
+            Assert.Equal(nameof(ConstraintsError.NamespaceNotFoundForLayer), result.Key);
+        }
+    
+        [Theory]
+        [InlineData(RuleOperator.NeedToRelate)]
+        [InlineData(RuleOperator.OnlyNeedToRelate)]
+        [Trait(nameof(ConstraintsAndViolationsMethods.GetViolations), "Success_NeedToRelate")]
+        public void GetViolations_Success_NeedToRelate(RuleOperator ruleOperator)
         {
             var originLayer = "Origin";
             var targetLayer = "Target";
@@ -119,7 +160,7 @@ namespace ErosionFinder.Tests
                     {
                         OriginLayer = originLayer,
                         TargetLayer = targetLayer,
-                        RuleOperator = RuleOperator.NeedToRelate
+                        RuleOperator = ruleOperator
                     }
                 },
                 Layers = new Dictionary<string, NamespacesGroupingMethod>()
@@ -131,8 +172,8 @@ namespace ErosionFinder.Tests
 
             var codeFiles = new List<CodeFile>()
             {
-                GetClassCodeFileWithoutReferencesAndRelations(originStructureName, originNamespace),
-                GetClassCodeFileWithoutReferencesAndRelations(targetStructureName, targetNamespace)
+                GetCodeFileWithSingleClassStructure(originStructureName, originNamespace, null, null),
+                GetCodeFileWithSingleClassStructure(targetStructureName, targetNamespace, null, null)
             };
 
             var violations = ConstraintsAndViolationsMethods.GetViolations(
@@ -146,22 +187,140 @@ namespace ErosionFinder.Tests
 
         }    
 
+        [Fact]
+        [Trait(nameof(ConstraintsAndViolationsMethods.GetViolations), "Success_OnlyCanRelate")]
+        public void GetViolations_Success_OnlyCanRelate()
+        {
+            var originLayer = "Origin";
+            var targetLayer = "Target";
+            var thirdLayer = "Third";
+
+            var originNamespace = "Test.Origin";
+            var targetNamespace = "Test.Target";
+            var thirdNamespace = "Test.Third";
+
+            var originStructureName = "OriginTestStructure";
+            var targetStructureName = "TargetTestStructure";
+            var thirdStructureName = "ThirdTestStructure";
+
+            var constraints = new ArchitecturalConstraints()
+            {
+                Rules = new List<ArchitecturalRule>()
+                {
+                    new ArchitecturalRule()
+                    {
+                        OriginLayer = originLayer,
+                        TargetLayer = targetLayer,
+                        RuleOperator = RuleOperator.OnlyCanRelate
+                    }
+                },
+                Layers = new Dictionary<string, NamespacesGroupingMethod>()
+                {
+                    { originLayer, GetNamespaceGroup(originNamespace) },
+                    { targetLayer, GetNamespaceGroup(targetNamespace) }
+                }
+            };
+
+            var thirdCodeFile = GetCodeFileWithSingleClassStructure(
+                thirdStructureName, 
+                thirdNamespace, 
+                new List<string>() { targetNamespace },
+                new List<Relation>()
+                {
+                    new Relation(RelationType.Inheritance, targetNamespace, true)
+                });
+
+            var codeFiles = new List<CodeFile>()
+            {
+                GetCodeFileWithSingleClassStructure(originStructureName, originNamespace, null, null),
+                GetCodeFileWithSingleClassStructure(targetStructureName, targetNamespace, null, null),
+                thirdCodeFile
+            };
+
+            var violations = ConstraintsAndViolationsMethods.GetViolations(
+                constraints, codeFiles, default);
+
+            Assert.NotNull(violations);
+            Assert.Single(violations);
+            Assert.Single(violations.Single().Structures);
+            Assert.Equal(constraints.Rules.Single().RuleOperator, violations.First().Rule.RuleOperator);
+            Assert.Equal(thirdStructureName, violations.First().Structures.Single());
+        }    
+
+        [Fact]
+        [Trait(nameof(ConstraintsAndViolationsMethods.GetViolations), "Success_CanNotRelate")]
+        public void GetViolations_Success_CanNotRelate()
+        {
+            var originLayer = "Origin";
+            var targetLayer = "Target";
+            
+            var originNamespace = "Test.Origin";
+            var targetNamespace = "Test.Target";
+            
+            var originStructureName = "OriginTestStructure";
+            var targetStructureName = "TargetTestStructure";
+            
+            var constraints = new ArchitecturalConstraints()
+            {
+                Rules = new List<ArchitecturalRule>()
+                {
+                    new ArchitecturalRule()
+                    {
+                        OriginLayer = originLayer,
+                        TargetLayer = targetLayer,
+                        RuleOperator = RuleOperator.CanNotRelate
+                    }
+                },
+                Layers = new Dictionary<string, NamespacesGroupingMethod>()
+                {
+                    { originLayer, GetNamespaceGroup(originNamespace) },
+                    { targetLayer, GetNamespaceGroup(targetNamespace) }
+                }
+            };
+
+            var firstCodeFile = GetCodeFileWithSingleClassStructure(
+                originStructureName, 
+                originNamespace, 
+                new List<string>() { targetNamespace },
+                new List<Relation>()
+                {
+                    new Relation(RelationType.Inheritance, targetNamespace, true)
+                });
+
+            var codeFiles = new List<CodeFile>()
+            {
+                firstCodeFile,
+                GetCodeFileWithSingleClassStructure(targetStructureName, targetNamespace, null, null)
+            };
+
+            var violations = ConstraintsAndViolationsMethods.GetViolations(
+                constraints, codeFiles, default);
+
+            Assert.NotNull(violations);
+            Assert.Single(violations);
+            Assert.Single(violations.Single().Structures);
+            Assert.Equal(constraints.Rules.Single().RuleOperator, violations.First().Rule.RuleOperator);
+            Assert.Equal(originStructureName, violations.First().Structures.Single());
+        }    
+
         private NamespacesExplicitlyGrouped GetNamespaceGroup(string fullNamespace)
             => new NamespacesExplicitlyGrouped(new List<string>() { fullNamespace });
 
-        private CodeFile GetClassCodeFileWithoutReferencesAndRelations(
-            string name, string fullNamespace)
-                => new CodeFile()
+        private CodeFile GetCodeFileWithSingleClassStructure(string name, string fullNamespace, 
+            IEnumerable<string> references, IEnumerable<Relation> relations)
+            => new CodeFile()
+            {
+                Structures = new List<Structure>()
                 {
-                    Structures = new List<Structure>()
+                    new Structure()
                     {
-                        new Structure()
-                        {
-                            Name = name,
-                            Type = StructureType.Class,
-                            Namespace = fullNamespace
-                        }
+                        Name = name,
+                        Type = StructureType.Class,
+                        Namespace = fullNamespace,
+                        References = references,
+                        Relations = relations
                     }
-                };
+                }
+            };
     }
 }
