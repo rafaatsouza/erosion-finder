ffda8236f6551df05a5ba1d9b79deb19b9240eeb
Rafael Torres <souza.t.rafael@gmail.com>
2020-12-20 18:57:09 -0300

    Fix encoding and adds 'TODO'

--- Source/ErosionFinder/Extensions/ArchitecturalRuleExtensions.cs
+++ Source/ErosionFinder/Extensions/ArchitecturalRuleExtensions.cs
@@ -1,24 +1,27 @@
-using ErosionFinder.Data.Models;
 using ErosionFinder.Data.Exceptions;
-using System.Collections.Generic;
+using ErosionFinder.Data.Models;
 using ErosionFinder.Dtos;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ErosionFinder.Extensions
 {
     internal static class ArchitecturalRuleExtensions
     {
-        public static void CheckIfItsValid(this ArchitecturalRule rule)
+        public static void CheckIfItsValid(
+            this ArchitecturalRule rule)
         {
-            if (rule == null)
-                return;
-
-           if (string.IsNullOrEmpty(rule.OriginLayer)
-            || string.IsNullOrEmpty(rule.TargetLayer)
-            || rule.RuleOperator == default(RuleOperator)) 
+            if (rule != null)
             {
-                throw new ConstraintsException(
-                    ConstraintsError.InvalidRule);
+                var invalidRule = string.IsNullOrEmpty(rule.OriginLayer) 
+                    || string.IsNullOrEmpty(rule.TargetLayer) 
+                    || rule.RuleOperator == default(RuleOperator);
+
+                if (invalidRule)
+                {
+                    throw new ConstraintsException(
+                        ConstraintsError.InvalidRule);
+                }
             }
         }
 
@@ -31,33 +34,52 @@ namespace ErosionFinder.Extensions
 
             rule.CheckIfItsValid();
 
-            switch (rule.RuleOperator)
+            var violatingStructures = GetViolatingStructuresByNamespaces(
+                rule.RuleOperator, targetNamespaces, originStructures, anotherStructures);
+
+            if (rule.HasParticularRelationTypes)
             {
-                case (RuleOperator.NeedToRelate):
-                    return GetNeedToRelateViolatingStructures(
-                        targetNamespaces, originStructures);
-
-                case (RuleOperator.OnlyNeedToRelate):
-                    return GetOnlyNeedToRelateViolatingStructures(
-                        targetNamespaces, originStructures, anotherStructures);
-
-                case (RuleOperator.OnlyCanRelate):
-                    return GetOnlyCanRelateViolatingStructures(
-                        targetNamespaces, anotherStructures);
-
-                case (RuleOperator.CanNotRelate):
-                    return GetCanNotRelateViolatingStructures(
-                        targetNamespaces, originStructures);
+                //TODO: Filter violating by particular relation types
             }
 
+            return violatingStructures;
+        }
+
+        private static IEnumerable<Structure> GetViolatingStructuresByNamespaces(
+            RuleOperator ruleOperator, IEnumerable<string> targetNamespaces, 
+            IEnumerable<Structure> originStructures, IEnumerable<Structure> anotherStructures)
+        {
+            if (ruleOperator == RuleOperator.NeedToRelate)
+            {
+                return GetNeedToRelateViolatingStructures(
+                    targetNamespaces, originStructures);
+            }
+            else if (ruleOperator == RuleOperator.OnlyNeedToRelate)
+            {
+                return GetOnlyNeedToRelateViolatingStructures(
+                    targetNamespaces, originStructures, anotherStructures);
+            }
+            else if (ruleOperator == RuleOperator.OnlyCanRelate)
+            {
+                return GetOnlyCanRelateViolatingStructures(
+                    targetNamespaces, anotherStructures);
+            }
+            else if (ruleOperator == RuleOperator.CanNotRelate)
+            {
+                return GetCanNotRelateViolatingStructures(
+                    targetNamespaces, originStructures);
+            }
+            
             return Enumerable.Empty<Structure>();
         }
 
         private static IEnumerable<Structure> GetNeedToRelateViolatingStructures(
             IEnumerable<string> targetNamespaces, IEnumerable<Structure> originStructures)
-            => originStructures
+        {
+            return originStructures
                 .Where(s => !s.Relations
                     .Any(r => targetNamespaces.Any(n => n.Equals(r.Target))));
+        }
 
         private static IEnumerable<Structure> GetOnlyNeedToRelateViolatingStructures(
             IEnumerable<string> targetNamespaces, IEnumerable<Structure> originStructures,
@@ -76,14 +98,18 @@ namespace ErosionFinder.Extensions
 
         private static IEnumerable<Structure> GetOnlyCanRelateViolatingStructures(
             IEnumerable<string> targetNamespaces, IEnumerable<Structure> anotherStructures)
-            => anotherStructures
+        {
+            return anotherStructures
                 .Where(s => s.Relations
                     .Any(r => targetNamespaces.Any(n => n.Equals(r.Target))));
+        }
 
         private static IEnumerable<Structure> GetCanNotRelateViolatingStructures(
             IEnumerable<string> targetNamespaces, IEnumerable<Structure> originStructures)
-            => originStructures
+        {
+            return originStructures
                 .Where(s => s.Relations
                     .Any(r => targetNamespaces.Any(n => n.Equals(r.Target))));
+        }
     }
 }
