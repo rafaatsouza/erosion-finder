aae1e9c4b4f3e09d5109eec9b03e660dc4246beb
Rafael Torres <souza.t.rafael@gmail.com>
2020-12-25 16:54:34 -0300

    Adds extension method to check if two rules are the same

--- Source/ErosionFinder/Extensions/ArchitecturalRuleExtensions.cs
+++ Source/ErosionFinder/Extensions/ArchitecturalRuleExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using ErosionFinder.Data.Exceptions;
 using ErosionFinder.Data.Models;
 
@@ -21,5 +24,28 @@ namespace ErosionFinder.Extensions
                 }
             }
         }
+
+        public static bool Equals(this ArchitecturalRule mainRule, 
+            ArchitecturalRule targetRule)
+        {
+            if (mainRule == null && targetRule == null)
+                return true;
+
+            if (mainRule == null || targetRule == null)
+                return false;
+
+            if (mainRule.RelationTypes.Count() != targetRule.RelationTypes.Count())
+                return false;
+
+            return TextEquals(mainRule.OriginLayer, targetRule.OriginLayer)
+                && TextEquals(mainRule.TargetLayer, targetRule.TargetLayer)
+                && mainRule.RuleOperator == targetRule.RuleOperator
+                && !mainRule.RelationTypes
+                    .Any(rt => !targetRule.RelationTypes.Any(rtt => rtt == rt));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool TextEquals(string text, string anotherText)
+            => string.Equals(text, anotherText, StringComparison.InvariantCultureIgnoreCase);
     }
 }
